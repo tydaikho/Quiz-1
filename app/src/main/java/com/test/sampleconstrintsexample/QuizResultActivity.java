987363e09b09db211f19318d1f344170c047dae4
moxDroid <pritam.world@gmail.com>
2017-04-17 21:13:47 -0400

    Result screen updated

--- app/src/main/java/com/test/sampleconstrintsexample/QuizResultActivity.java
+++ app/src/main/java/com/test/sampleconstrintsexample/QuizResultActivity.java
@@ -1,12 +1,19 @@
 package com.test.sampleconstrintsexample;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class QuizResultActivity extends AppCompatActivity
 {
+    TextView txtScore, txtResult, txtGreetings;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -15,8 +22,26 @@ public class QuizResultActivity extends AppCompatActivity
 
         intiActionBar();
 
+        txtScore = (TextView)findViewById(R.id.txtScore);
+        txtResult = (TextView)findViewById(R.id.txtResult);
+        txtGreetings = (TextView)findViewById(R.id.txtGreetings);
+
         int score = getIntent().getExtras().getInt("result",0);
-        Toast.makeText(this,"Score : " + score,Toast.LENGTH_LONG).show();
+        txtScore.setText("Score : " + score);
+        blinkText();
+        if(score <5)
+        {
+            txtScore.setTextColor(Color.RED);
+            txtResult.setText("Fail");
+            txtGreetings.setText("Bad Luck!!!");
+        }
+        else
+        {
+            txtScore.setTextColor(Color.GREEN);
+            txtResult.setText("Pass");
+            txtGreetings.setText("Congratulation!!!");
+        }
+        //Toast.makeText(this,"Score : " + score,Toast.LENGTH_LONG).show();
     }
 
     private void intiActionBar()
@@ -25,5 +50,14 @@ public class QuizResultActivity extends AppCompatActivity
         actionBar.setLogo(R.drawable.trophy);
         actionBar.setDisplayUseLogoEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
+    }
+
+    private void blinkText(){
+
+        AlphaAnimation anim = new AlphaAnimation(0.0f, 1.0f);
+        anim.setDuration(50); //You can manage the blinking time with this parameter
+        anim.setStartOffset(20);
+        anim.setRepeatMode(Animation.REVERSE);
+        txtGreetings.setAnimation(anim);
     }
 }
